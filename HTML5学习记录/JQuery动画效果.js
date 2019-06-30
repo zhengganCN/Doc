1f7ed475929f3bb2050e8e2747e13bcc10a5662a
zhengganCN <932813593@qq.com>
2019-06-30 14:36:08 +0800

    学习

---
+++ HTML5学习记录/JQuery动画效果.js
@@ -0,0 +1,4 @@
+$(selector).animate({
+    marginLeft:200,
+    marginTop:200}
+    ,2000);  //执行时间，ms
---
+++ HTML5学习记录/点击文本框时使用js选中文本框中的内容.js
@@ -0,0 +1,5 @@
+$(function () {
+    $("#search_text>input").click(function(){
+        this.select();
+    });
+});
